874a4daee497b08e770b00b846fc280702cae07f
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-11-01 11:19:55 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,24 +14,29 @@ basic.showIcon(IconNames.Happy)
 basic.pause(500)
 basic.clearScreen()
 
-// Define the perimeter positions
-const perimeter = [
-    [0, 0], [1, 0], [2, 0], [3, 0], [4, 0],
-    [4, 1], [4, 2], [4, 3], [4, 4], 
-    [3, 4], [2, 4], [1, 4], [0, 4], 
-    [0, 3], [0, 2], [0, 1]  
-]
-
 // when "A" is pressed, the sprite moves clockwise
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     loopCounter = 0
-    sprite = game.createSprite(perimeter[loopCounter][0], perimeter[loopCounter][1])
+    sprite = game.createSprite([loopCounter][0], [loopCounter][1])
 
-    while (loopCounter < perimeter.length) {
-        sprite.set(LedSpriteProperty.X, perimeter[loopCounter][0])
-        sprite.set(LedSpriteProperty.Y, perimeter[loopCounter][1])
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, [loopCounter][0])
         loopCounter += 1
+        basic.pause(500)
+    }
+    
+    loopCounter = 0
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.Y, [loopCounter][0])
+        loopCounter += 1
+        basic.pause(500)
+    }
+
+    loopCounter = 0
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, [loopCounter][0])
+        loopCounter =- 1
         basic.pause(500)
     }
     sprite.delete()
@@ -42,12 +47,11 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     // setup
     basic.clearScreen()
-    loopCounter = perimeter.length - 1
-    sprite = game.createSprite(perimeter[loopCounter][0], perimeter[loopCounter][1])
+    loopCounter -= 1
+    sprite = game.createSprite([loopCounter][0], [loopCounter][1])
 
-    while (loopCounter >= 0) {
-        sprite.set(LedSpriteProperty.X, perimeter[loopCounter][0])
-        sprite.set(LedSpriteProperty.Y, perimeter[loopCounter][1])
+    while (loopCounter >= 5) {
+        sprite.set(LedSpriteProperty.X, [loopCounter][0])
         loopCounter -= 1
         basic.pause(500)
     }
