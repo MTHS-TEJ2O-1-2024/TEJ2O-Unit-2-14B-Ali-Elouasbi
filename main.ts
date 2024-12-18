7db0cbebdd38a487bf88b6acc93bdfad0f79b622
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-12-18 18:52:47 -0500

    code

--- main.ts
+++ main.ts
@@ -5,54 +5,77 @@
  * This program shows while loops in action
 */
 
-// variables
+// Variables
 let sprite: game.LedSprite = null
 let loopCounter = 0
 
-// setup
+// Setup
 basic.showIcon(IconNames.Happy)
 basic.pause(500)
 basic.clearScreen()
 
-// when "A" is pressed, the sprite moves clockwise
+// When "A" is pressed, the pixels move around the perimeter clockwise
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    loopCounter = 0
-    sprite = game.createSprite([loopCounter][0], [loopCounter][1])
-
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.X, [loopCounter][0])
-        loopCounter += 1
-        basic.pause(500)
-    }
     
+    // Setup
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
     loopCounter = 0
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.Y, [loopCounter][0])
-        loopCounter += 1
-        basic.pause(500)
-    }
 
-    loopCounter = 0
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.X, [loopCounter][0])
-        loopCounter =- 1
+    while (loopCounter < 16) {
+        // Move clockwise around the perimeter
+        if (loopCounter < 5) {
+            // Top
+            sprite.set(LedSpriteProperty.X, loopCounter)
+            sprite.set(LedSpriteProperty.Y, 0)
+        } else if (loopCounter < 9) {
+            // Right side
+            sprite.set(LedSpriteProperty.X, 4)
+            sprite.set(LedSpriteProperty.Y, loopCounter - 4)
+        } else if (loopCounter < 13) {
+            // Bottom
+            sprite.set(LedSpriteProperty.X, 12 - loopCounter)
+            sprite.set(LedSpriteProperty.Y, 4)
+        } else {
+            // Left side
+            sprite.set(LedSpriteProperty.X, 0)
+            sprite.set(LedSpriteProperty.Y, 16 - loopCounter)
+        }
+        loopCounter = loopCounter + 1
         basic.pause(500)
     }
     sprite.delete()
     basic.showIcon(IconNames.Happy)
 })
 
-// when "B" is pressed, the sprite moves counter-clockwise
+// When "B" is pressed, the pixels move around the perimeter counterclockwise
 input.onButtonPressed(Button.B, function () {
-    // setup
+    
+    //Setup
     basic.clearScreen()
-    loopCounter -= 1
-    sprite = game.createSprite([loopCounter][0], [loopCounter][1])
+    sprite = game.createSprite(0, 0)
+    loopCounter = 16
 
-    while (loopCounter >= 5) {
-        sprite.set(LedSpriteProperty.X, [loopCounter][0])
-        loopCounter -= 1
+    while (loopCounter >= 0) {
+        // Move counterclockwise around the perimeter
+        if (loopCounter < 5) {
+            // Top
+            sprite.set(LedSpriteProperty.X, loopCounter)
+            sprite.set(LedSpriteProperty.Y, 0)
+        } else if (loopCounter < 9) {
+            // Right side
+            sprite.set(LedSpriteProperty.X, 4)
+            sprite.set(LedSpriteProperty.Y, loopCounter - 4)
+        } else if (loopCounter < 13) {
+            // Bottom
+            sprite.set(LedSpriteProperty.X, 12 - loopCounter)
+            sprite.set(LedSpriteProperty.Y, 4)
+        } else {
+            // Left side
+            sprite.set(LedSpriteProperty.X, 0)
+            sprite.set(LedSpriteProperty.Y, 16 - loopCounter)
+        }
+        loopCounter = loopCounter - 1
         basic.pause(500)
     }
     sprite.delete()
