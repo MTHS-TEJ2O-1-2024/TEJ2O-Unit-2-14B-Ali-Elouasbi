85c1671216a7eba0e65117c474be91fbbcb36473
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-12-20 11:16:53 -0500

    comments

--- main.ts
+++ main.ts
@@ -16,8 +16,8 @@ basic.clearScreen()
 
 // When "A" is pressed, the pixels move around the perimeter clockwise
 input.onButtonPressed(Button.A, function () {
-    
-    // Setup
+
+  // Setup
     basic.clearScreen()
     sprite = game.createSprite(0, 0)
     loopCounter = 0
@@ -50,28 +50,24 @@ input.onButtonPressed(Button.A, function () {
 
 // When "B" is pressed, the pixels move around the perimeter counterclockwise
 input.onButtonPressed(Button.B, function () {
-    
+
     //Setup
     basic.clearScreen()
     sprite = game.createSprite(0, 0)
     loopCounter = 16
 
     while (loopCounter >= 0) {
-        // Move counterclockwise around the perimeter
+        // Moves sprite
         if (loopCounter < 5) {
-            // Top
             sprite.set(LedSpriteProperty.X, loopCounter)
             sprite.set(LedSpriteProperty.Y, 0)
         } else if (loopCounter < 9) {
-            // Right side
             sprite.set(LedSpriteProperty.X, 4)
             sprite.set(LedSpriteProperty.Y, loopCounter - 4)
         } else if (loopCounter < 13) {
-            // Bottom
             sprite.set(LedSpriteProperty.X, 12 - loopCounter)
             sprite.set(LedSpriteProperty.Y, 4)
         } else {
-            // Left side
             sprite.set(LedSpriteProperty.X, 0)
             sprite.set(LedSpriteProperty.Y, 16 - loopCounter)
         }
